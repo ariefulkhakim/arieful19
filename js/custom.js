bbea3784cfca07ef8e2e2464257ac9bb69c132e9
ariefulkhakim <ariefulkhakim24@gmail.com>
2017-02-02 13:16:57 +0700

    creating navigation

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+
+(function ($) {
+ "use strict";
+ 	// ==========Navigation==========
+ 	$('a.page-scroll').on('click', function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top - 50
+		}, 1500);
+		e.preventDefault();
+	});		
+
+	$(window).on('scroll', function() {
+	  if ($(this).scrollTop() > 100) {
+		$('.menu-top').addClass('menu-show');
+	  } else {
+		$('.menu-top').removeClass('menu-show');
+	  }
+	});
+	
+	$(document).on('click','.navbar-collapse.in',function(e) {
+	if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+		$(this).collapse('hide');
+	}
+	});				
+
+
+
+	
+	new WOW().init();	
+	
+})(jQuery);    
+
+
